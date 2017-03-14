9cdc786ba14220934e2bf5759e362d0765ebc0ce
Abimael Luis <abimael@samas.mx>
2017-03-14 14:05:06 -0600

    First Commit, adding skeleton

---
+++ src/main/java/mx/neftaly/DemoPipelineApplication.java
@@ -0,0 +1,12 @@
+package mx.neftaly;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoPipelineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoPipelineApplication.class, args);
+	}
+}
